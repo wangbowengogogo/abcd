21c067687c8910e012711976546a8a1320f808ec
wangbowen <59051935+wangbowengogogo@users.noreply.github.com>
2020-07-23 15:37:05 +0800

    Add files via upload

--- 网易云课堂/js/index.js
+++ 网易云课堂/js/index.js
@@ -22,18 +22,27 @@ $(function () {
 
     // 隐藏首页的课程导航的隐藏和出现
     $('.header .course').mouseover(function () {
-        $(this).children('.course1').stop().slideToggle();
+        $(this).children('.course1').show();
     });
+	$('.header .course').mouseout(function () {
+	    $(this).children('.course1').hide();
+	});
 
     // 隐藏首页的关注领福利二维码的隐藏和出现
     $('.header .welfare').mouseover(function () {
-        $(this).children('.ewm').stop().slideToggle();
+        $(this).children('.ewm').show();
     });
+	$('.header .welfare').mouseout(function () {
+	    $(this).children('.ewm').hide();
+	});
 
     // 搜索 课程的隐藏和出现
     $('.header .search>div').mouseover(function () {
-        $(this).children('ul').stop().slideToggle();
+        $(this).children('ul').show();
     })
+	$('.header .search>div').mouseover(function () {
+	    $(this).children('ul').hide();
+	})
 
 
     // $(".nav>li").hover(function () {
@@ -42,8 +51,14 @@ $(function () {
     //   });
 
 
-
-
+$("#uls>li").click(function(){
+				var index = $(this).index()
+				
+				$(this).css("background","#49AF4F").siblings(".lis").css("background","#ECF7ED");
+//				获取某个元素在同级标签中的索引值
+//				console.log($(this).index());
+				$(".divs").eq(index).css("display","inline-block").siblings(".divs").css("display","none");
+			})
 
 
 
